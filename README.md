39de5bfe5c7a92d9d7b1c3db716eefe0ed1c54eb
naheri <naheriahamada@gmail.com>
2023-01-15 16:22:28 +0100

    add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simplified Gender Detection webapp
+
+## Presentation
+
+Gender Detection Webapp, without the webcam part.
--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask,render_template,request
 import numpy as np
 from keras.models import load_model
-from keras.utils import img_to_array, load_img
+from keras.utils import img_to_array
 import os
-from PIL import Image
-import os.path as op
+import cv2
+import cvlib as cv
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'static/images/uploads'
+UPLOAD_FOLDER = 'static/images/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # dimensions of images
@@ -20,12 +20,60 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 loop_running = False
 
-def getPrediction(image_path):
-    image_path = f'static/images/{image_path}'
-    img = np.asarray(Image.open(image_path).resize((img_width, img_height)))
-    img = img/255
-    img = np.expand_dims(img, axis=0)
-    return 'female' if model.predict(img) == 0 else 'male'
+def get_gender(frame):
+    # load and resize image to 64x64
+    frame = cv2.resize(frame, (img_width, img_height))
+    # convert frame to numpy array
+    frame = img_to_array(frame)
+    # expand dimension of frame
+    frame = np.expand_dims(frame, axis=0)
+    print(model.predict(frame)[0][0])
+    # making prediction with model
+    return 'female' if model.predict(frame)[0][0] < 0.5 else 'male'
+
+
+def output_image(image_path):
+    predictions = []
+    global n_faces
+    image = cv2.imread(image_path)
+    # apply face detection
+    face, confidence = cv.detect_face(image)
+
+    if len(face) <= 0:
+        print('No face detected')
+    # loop through detected faces
+    else:
+        print(f'{len(face)} faces detected')
+        n_faces = len(face)
+    for f in face:
+        # get corner points of face rectangle        
+        (startX, startY) = f[0], f[1]
+        (endX, endY) = f[2], f[3]
+
+        # draw rectangle over face
+        cv2.rectangle(image, (startX,startY), (endX,endY), (0,255,0), 2)
+
+        # crop the detected face region
+        face_crop = np.copy(image[startY:endY,startX:endX])
+        if (face_crop.shape[0]) < 10 or (face_crop.shape[1]) < 10:
+            continue
+
+        
+        Y = startY - 10 if startY > 20 else startY + 10
+        prediction = get_gender(face_crop)
+        print(prediction)
+        predictions.append(prediction)
+        cv2.putText(image, prediction, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7, (0, 255, 0), 2)
+
+
+
+    if(cv2.imwrite('static/images/detected_faces.jpg', image) == False):
+        print("Error saving the image")
+    else:
+        print('image saved')
+    return 'static/images/detected_faces.jpg'
+
 
 @app.route("/")
 def index():
@@ -39,11 +87,9 @@ def get_gender_from_image():
         if file:
             filename = secure_filename(file.filename)
             file.save(os.path.join('static/images', filename))
-            path = op.join('static/images', filename)
+            path = os.path.join('static/images', filename)
     # remove the image from the static/images folder
-    os.remove(path)
-
-    return render_template('image_response.html', gender=getPrediction(path), image_path=path)
+    return render_template('image_response.html', image_path=output_image(path), number_of_people=n_faces)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
